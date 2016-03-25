37878b7a9167bece5924b314a4cd2a4d525820bd
Nejc Sever <nejc.sever@outfit7.com>
2016-03-25 21:09:06 +0100

    make items clickable

--- src/angular-list-select.js
+++ src/angular-list-select.js
@@ -7,21 +7,33 @@
 (function() {
     angular.module('angular-list-select', [])
         .directive('listSelect', function() {
-            return {
-                restrict: 'AE',
+            var directive = {
+                restrict: 'EA',
                 replace: true,
                 scope: {
                     items: '=',
                     selectedItems: '='
                 },
+                link: linkFunc,
                 template: '<div class="ls-container">' +
                     '<div class="ls-items">' +
-                    '<div class="ls-item" ng-repeat="item in items">{{item.name}}</div>' +
+                    '<div class="ls-item" ng-repeat="item in items" ng-click="itemClick(item)">{{item.name}}</div>' +
                     '</div>' +
                     '<div class="ls-selected-items">' +
-                    '<div class="ls-selected-item" ng-repeat="item in selectedItems">{{item.name}}</div>' +
+                    '<div class="ls-selected-item" ng-repeat="item in selectedItems" ng-click="selItemClick(item)">{{item.name}}</div>' +
                     '</div>' +
                     '</div>'
+            }
+
+            return directive;
+
+            function linkFunc(scope, el, attr, ctrl) {
+                scope.itemClick = function(item) {
+                    console.log(item);
+                };
+                scope.selItemClick = function(item) {
+                    console.log(item);
+                };
             }
         });
 })();
